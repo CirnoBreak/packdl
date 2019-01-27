1cd26b47eb51e555fe8dc9f48977f7de33625af3
CirnoBreak <1103054869@qq.com>
2019-01-27 15:14:10 +0800

    change search type to input search with debounce

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,28 +1,47 @@
 /**
  * 搜索组件
  */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Row, Input } from 'antd';
 import { bindActionCreators } from 'redux';
 import { inputSearch } from '../store/actions'
+import { useDebounce } from 'use-debounce';
 
-// 获取antd Search组件
-const Search = Input.Search;
+/**
+ * 搜索操作,防抖500毫秒执行
+ * @param {function} inputSearch 搜索的action
+ * @param {string} searchText 搜索的关键词
+ */
+function useSearch(inputSearch, searchText) {
+  const debounceSearchText = useDebounce(searchText, 500);
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(
+    () => {
+      isMounted ?
+        inputSearch(debounceSearchText) :
+        setIsMounted(true)
+    },
+    [debounceSearchText]
+  );
+  return debounceSearchText;
+}
 
 /**
  * 搜索组件
  * @param {function} inputSearch 输入关键词后按下回车或者图标后搜索的action 
  */
 function SearchInput({ inputSearch }) {
+  const [searchText, setSearchText] = useState('');
+  useSearch(inputSearch, searchText);
   return (
     <Row type="flex" justify="end">
-      <Search
+      <Input
         style={{ marginTop: 50, marginBottom: 20, width: 300, marginRight: 10}}
         placeholder="input the pack name."
-        onSearch={(v, e) => inputSearch(v)}
+        onChange={(e) => setSearchText(e.target.value)}
         >
-      </Search>
+      </Input>
     </Row>
   )
 };
